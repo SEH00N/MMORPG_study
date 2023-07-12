e0cbec84563acde85feca2bae6a80766877f4761
SEH00N <06psh0501@gmail.com>
2023-07-13 01:52:18 +0900

    csharp basic syntax & text rpg

---
+++ CSharpBasicSyntax/CSharpBasicSyntax/Function.cs
@@ -0,0 +1,89 @@
+﻿namespace CSharpBasicSyntax
+{
+    class Function
+    {
+        static int Add(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+
+        static int Add(int a, int b, int c = 0)
+        {
+            int result = a + b + c;
+            return result;
+        }
+
+        static float Add(float a, float b)
+        {
+            float result = a + b;
+            return result;
+        }
+
+        static void AddOne(ref int a)
+        {
+            a = a + 1;
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void Divide(int a, int b, out int result)
+        {
+            result = a / b;
+        }
+
+        static int Factorial(int n)
+        {
+            //int result = 1;
+            //for(int i = 2; i < n + 1; i++)
+            //    result *= i;
+
+            //return result;
+
+            if (n <= 2)
+                return n;
+
+            return n * Factorial(n - 1);
+        }
+
+        //static void Main(string[] args)
+        //{
+        //    //int result = Function.Add(4, 5);
+        //    //Console.WriteLine(result);
+
+        //    //int a = 0;
+        //    //Function.AddOne(ref a);
+        //    //Console.WriteLine(a);
+
+        //    //int a = 3, b = 5;
+        //    //Swap(ref a, ref b);
+        //    //Console.WriteLine($"a: {a}, b: {b}");
+
+        //    //int a = 6, b = 3;
+        //    //Divide(a, b, out int result);
+        //    //Console.WriteLine(result);
+
+        //    //float result = Add(1f, 3f);
+        //    //Console.WriteLine(result);
+
+        //    //for(int i = 2; i < 10; i++)
+        //    //    for (int j = 1; j < 10; j++)
+        //    //        Console.WriteLine($"{i} x {j} = {i*j}");
+
+        //    //for(int i = 0; i < 5; i++)
+        //    //{
+        //    //    for(int j = 0; j < i + 1; j++)
+        //    //        Console.Write('*');
+
+        //    //    Console.WriteLine();
+        //    //}
+
+        //    Console.WriteLine(Factorial(3));
+        //}
+    }
+}
---
+++ CSharpBasicSyntax/CSharpBasicSyntax/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace CSharpBasicSyntax
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    // 0: 가위    1: 바위   2: 보
+
+        //    Random random = new Random();
+        //    int aiChoice = random.Next(0, 3);
+        //    int choice = Convert.ToInt32(Console.ReadLine());
+
+        //    switch(choice)
+        //    {
+        //        case 0:
+        //            Console.WriteLine("당신의 선택은 가위입니다.");
+        //            break;
+        //        case 1:
+        //            Console.WriteLine("당신의 선택은 바위입니다.");
+        //            break;
+        //        case 2:
+        //            Console.WriteLine("당신의 선택은 보입니다.");
+        //            break;
+        //    }
+
+        //    switch(aiChoice)
+        //    {
+        //        case 0:
+        //            Console.WriteLine("AI의 선택은 가위입니다.");
+        //            break;
+        //        case 1:
+        //            Console.WriteLine("AI의 선택은 바위입니다.");
+        //            break;
+        //        case 2:
+        //            Console.WriteLine("AI의 선택은 보입니다.");
+        //            break;
+        //    }
+
+        //    if(choice == aiChoice)
+        //        Console.WriteLine("무승부입니다.");
+        //    else if((choice == 0 && aiChoice == 2) || (choice == 1 && aiChoice == 0) || (choice == 2 && aiChoice == 1))
+        //        Console.WriteLine("승리입니다.");
+        //    else
+        //        Console.WriteLine("패배입니다.");
+
+        //    //if(choice == 0)
+        //    //{
+        //    //    if (aiChoice == 0)
+        //    //        Console.WriteLine("무승부입니다.");
+        //    //    else if (aiChoice == 1)
+        //    //        Console.WriteLine("패배입니다.");
+        //    //    else
+        //    //        Console.WriteLine("승리입니다.");
+        //    //}
+        //    //else if(choice == 1)
+        //    //{
+        //    //    if (aiChoice == 0)
+        //    //        Console.WriteLine("승리입니다.");
+        //    //    else if (aiChoice == 1)
+        //    //        Console.WriteLine("무승부입니다.");
+        //    //    else
+        //    //        Console.WriteLine("패배입니다.");
+        //    //}
+        //    //else
+        //    //{
+        //    //    if (aiChoice == 0)
+        //    //        Console.WriteLine("패배입니다.");
+        //    //    else if (aiChoice == 1)
+        //    //        Console.WriteLine("승리입니다.");
+        //    //    else
+        //    //        Console.WriteLine("무승부입니다.");
+        //    //}
+        //}
+    }
+}
