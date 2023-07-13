fa8ca75002bff7dbb08c8a7b1facf053fb39c1e4
SEH00N <06psh0501@gmail.com>
2023-07-13 11:04:47 +0900

    reflection & nullable

---
+++ CSharpBasicSyntax/CSharpBasicSyntax/Nullable.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharpBasicSyntax
+{
+    internal class Nullable
+    { 
+        static void Main(string[] args)
+        {
+            int? number = 5;
+            int b = number ?? 0;
+            Console.WriteLine(b);
+
+            if(number != null)
+            {
+                int a = number.Value;
+                Console.WriteLine(a);
+            }
+
+            if(number.HasValue)
+            {
+                int a = number.Value;
+                Console.WriteLine(a);
+            }
+
+        }
+    }
+}
--- CSharpBasicSyntax/CSharpBasicSyntax/Reflection.cs
+++ CSharpBasicSyntax/CSharpBasicSyntax/Reflection.cs
@@ -1,7 +1,43 @@
-﻿namespace CSharpBasicSyntax
+﻿using System.Reflection;
+
+namespace CSharpBasicSyntax
 {
     internal class Reflection
     {
+        class Important : Attribute
+        {
+            string message;
 
+            public Important(string message) { this.message = message; }
+        }
+
+        class Monster
+        {
+            [Important("Very Important")]
+            public int hp;
+            protected int attack;
+            private float speed;
+
+            void Attack() { }
+        }
+
+        //static void Main(string[] args)
+        //{
+        //    Monster monster = new Monster();
+        //    Type type = monster.GetType();
+
+        //    FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+
+        //    foreach(FieldInfo field in fields)
+        //    {
+        //        string access = "protected";
+        //        if (field.IsPublic)
+        //            access = "public";
+        //        else if (field.IsPrivate)
+        //            access = "private";
+
+        //        Console.WriteLine($"{access} {field.FieldType.Name} {field.Name}");
+        //    }
+        //}
     }
 }
