cd428dc491a628abb903f54f1403f94d84745d2d
SEH00N <06psh0501@gmail.com>
2023-07-13 10:21:18 +0900

    array

---
+++ CSharpBasicSyntax/CSharpBasicSyntax/2DArray.cs
@@ -0,0 +1,49 @@
+﻿namespace CSharpBasicSyntax
+{
+    internal class _2DArray
+    {
+        class Map
+        {
+            int[,] tiles = {
+                { 1, 1, 1, 1, 1 },
+                { 1, 0, 0, 0, 1 },
+                { 1, 0, 0, 0, 1 },
+                { 1, 0, 0, 0, 1 },
+                { 1, 1, 1, 1, 1 },
+            };
+
+            public void Render()
+            {
+                ConsoleColor defaultColor = Console.ForegroundColor;
+                for(int y = 0; y < tiles.GetLength(1); y++)
+                {
+                    for (int x = 0; x < tiles.GetLength(0); x++)
+                    {
+                        Console.ForegroundColor = (tiles[y, x] == 1) ? ConsoleColor.Red : ConsoleColor.Green;
+                        Console.Write('\u25cf');
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.ForegroundColor = defaultColor;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[2, 3] {
+                { 1, 2, 3 },
+                { 1, 2, 3 }
+            };
+            arr[0, 0] = 1;
+            arr[1, 0] = 1;
+
+            Map map = new Map();
+            map.Render();
+
+            // [. . .]
+            // [. . .]
+
+        }
+    }
+}
--- CSharpBasicSyntax/CSharpBasicSyntax/Array.cs
+++ CSharpBasicSyntax/CSharpBasicSyntax/Array.cs
@@ -4,27 +4,67 @@
     {
         static int GetHighestScore(int[] scores)
         {
-            return 0;
+            int result = 0;
+            for(int i = 1; i < scores.Length; i++)
+            {
+                if (scores[i] > scores[result])
+                    result = i;
+            }
+
+            return scores[result];
         }
 
         static int GetAverageScore(int[] scores)
         {
-            return 0;
+            if (scores.Length == 0)
+                return -1;
+
+            int result = 0;
+            for (int i = 0; i < scores.Length; i++)
+                result += scores[i];
+
+
+            result = result / scores.Length;
+
+            return result;
         }
 
         static int GetIndexOf(int[] scores, int value)
         {
-            return 0;
+            for (int i = 0; i < scores.Length; i++)
+                if (scores[i] == value)
+                    return i;
+            
+            return -1;
         }
 
         static void Sort(int[] scores)
         {
-
+            for(int i = 0; i < scores.Length; i++)
+            {
+                for(int j = i; j < scores.Length - i - 1; j++)
+                {
+                    if (scores[j] > scores[j + 1])
+                    {
+                        int temp = scores[j];
+                        scores[j] = scores[j + 1];
+                        scores[j + 1] = temp;
+                    }
+                }
+            }
         }
 
-        static void Main(string[] args)
-        {
-            int[] scores = new int[]
-        }
+        //static void Main(string[] args)
+        //{
+        //    int[] scores = new int[5] { 10, 30, 40, 20, 50 };
+
+        //    Console.WriteLine(GetHighestScore(scores));
+        //    Console.WriteLine(GetAverageScore(scores));
+        //    Console.WriteLine(GetIndexOf(scores, 20));
+
+        //    Sort(scores);
+        //    for(int i = 0; i < scores.Length; i++)
+        //        Console.WriteLine(scores[i]);
+        //}
     }
 }
